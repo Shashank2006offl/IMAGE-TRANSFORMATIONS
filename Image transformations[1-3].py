560bf10ef2b1ad231a3ab8a357f21a0ec1c4a31c
Shashank2006offl <147140026+Shashank2006offl@users.noreply.github.com>
2025-05-21 10:40:22 +0530

    Update Image transformations[1-3].py

--- Image transformations[1-3].py
+++ Image transformations[1-3].py
@@ -3,17 +3,16 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-input_img=cv2.imread("color image of flower.jpg")
-input_img=cv2.cvtColor(input_img,cv2.)
+
+input_img = cv2.imread("/content/old-rusty-fishing-boat-slope-along-shore-lake.jpg")
+input_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2RGB)
+
 plt.axis('off')
 plt.imshow(input_img)
 plt.show()
-rows,cols,dim=input_img.shape
-M=np.float32([[1,0,20],
-             [0,1,50],
-             [0,0,1]])
-translated_img=cv2.
-plt.axis('off')
+
+rows, cols, dim = input_img.shape
+M = np.float32([[1, 0, 2000], [0, 1, 50], [0, 0, 1]])
 
 
 
@@ -26,18 +25,35 @@ plt.show()
 
 
 ##iii)Image Shearing
-M_x=np.float32([[1,0.2,0],
-               [0,1,0],
-               [0,0,1]])
-M_y=np.
-sheared_img_xaxis=cv2.warpPerspective(input_img,M_x,(cols,rows))
-sheared_img_yaxis=
+M_x = np.float32([[1, 0.2, 0], [0, 1, 0], [0, 0, 1]])
+M_y = np.float32([[1, 0, 0], [0.2, 1, 0], [0, 0, 1]])
+
+sheared_img_xaxis = cv2.warpPerspective(input_img, M_x, (cols, rows))
+sheared_img_yaxis = cv2.warpPerspective(input_img, M_y, (cols, rows))
+
 plt.axis('off')
 plt.imshow(sheared_img_xaxis)
 plt.show()
-plt.axis
+
+plt.axis('off')
+plt.imshow(sheared_img_yaxis)
 plt.show()
 
-
-
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+##iv)Image Reflection
+input_image = cv2.imread("/content/old-rusty-fishing-boat-slope-along-shore-lake.jpg")
+input_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
+plt.axis("off")
+plt.imshow(input_image)
+plt.show()
+rows, cols, dim = input_image.shape
+M_x = np.float32([[1, 0, 0], [0, -1, rows], [0, 0, 1]])
+M_y = np.float32([[-1, 0, cols], [0, 1, 0], [0, 0, 1]])
+reflected_img_xaxis = cv2.warpPerspective(input_image, M_x, (cols, rows))
+reflected_img_yaxis = cv2.warpPerspective(input_image, M_y, (cols, rows))
+plt.imshow(reflected_img_xaxis)  
+plt.axis("off")
+plt.show()
 
